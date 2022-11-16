8d50512961bae6639643f7aae5d3b05cd030136f
DaDioDa <56485296+DaDioDa@users.noreply.github.com>
2022-11-16 20:30:22 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import controller.Controller;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Controller ctrl = new Controller();
+        ctrl.Say();
+    }
+}
---
+++ src/controller/Controller.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class Controller {
+
+    public void Say()
+    {
+        System.out.println("This is my controller!");
+    }
+}
