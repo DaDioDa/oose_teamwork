3c23c14dc3d7410d0a700ad61f7785c87914d376
cweiting212 <72126261+cweiting212@users.noreply.github.com>
2022-12-03 02:13:15 +0800

    Update Query.java

--- src/Query.java
+++ src/Query.java
@@ -20,10 +20,24 @@ public class Query {
                             user, password);
             st = conn.createStatement();  // 產生 SQL 敘述物件
 
-            rs = st.executeQuery("select * from main");// 下達 SQL 命令
-
+            rs = st.executeQuery("select * from sauce");// 下達 SQL 命令
+            System.out.printf("%s\t", "----------配醬----------");
+            System.out.printf("\n");
             while (rs.next()) {       // 擷取下一筆紀錄
+                System.out.printf("%s\t", rs.getString("name"));
+                if(rs.getBoolean("lunch")){ //lunch==1,回傳為午餐
+                    System.out.printf("%s\t", "午餐");
+                }
+                else{
+                    System.out.printf("%s\t", "晚餐");
+                }
+                System.out.printf("\n");
+            }
 
+            rs = st.executeQuery("select * from main");// 下達 SQL 命令
+            System.out.printf("%s\t", "----------主餐----------");
+            System.out.printf("\n");
+            while (rs.next()) {       // 擷取下一筆紀錄
                 System.out.printf("%s\t", rs.getString("name"));
                 System.out.printf("%s\t", rs.getInt("price"));
                 if(rs.getBoolean("lunch")){ //lunch==1,回傳為午餐
@@ -33,7 +47,51 @@ public class Query {
                     System.out.printf("%s\t", "晚餐");
                 }
                 System.out.printf("\n");
+            }
 
+            rs = st.executeQuery("select * from soup");// 下達 SQL 命令
+            System.out.printf("%s\t", "----------湯品----------");
+            System.out.printf("\n");
+            while (rs.next()) {
+                System.out.printf("%s\t", rs.getString("name"));
+                System.out.printf("%s\t", rs.getInt("price"));
+                if(rs.getBoolean("lunch")){ //lunch==1,回傳為午餐
+                    System.out.printf("%s\t", "午餐");
+                }
+                else{
+                    System.out.printf("%s\t", "晚餐");
+                }
+                System.out.printf("\n");
+            }
+
+            rs = st.executeQuery("select * from dessert");// 下達 SQL 命令
+            System.out.printf("%s\t", "----------點心----------");
+            System.out.printf("\n");
+            while (rs.next()) {
+                System.out.printf("%s\t", rs.getString("name"));
+                System.out.printf("%s\t", rs.getInt("price"));
+                if(rs.getBoolean("lunch")){ //lunch==1,回傳為午餐
+                    System.out.printf("%s\t", "午餐");
+                }
+                else{
+                    System.out.printf("%s\t", "晚餐");
+                }
+                System.out.printf("\n");
+            }
+
+            rs = st.executeQuery("select * from drinks");// 下達 SQL 命令
+            System.out.printf("%s\t", "----------飲料----------");
+            System.out.printf("\n");
+            while (rs.next()) {
+                System.out.printf("%s\t", rs.getString("name"));
+                System.out.printf("%s\t", rs.getInt("price"));
+                if(rs.getBoolean("lunch")){ //lunch==1,回傳為午餐
+                    System.out.printf("%s\t", "午餐");
+                }
+                else{
+                    System.out.printf("%s\t", "晚餐");
+                }
+                System.out.printf("\n");
             }
             if(conn != null && !conn.isClosed()) {
                 System.out.println("資料庫連線測試成功！");
